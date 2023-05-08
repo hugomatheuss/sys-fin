df2600f4cf3a1396c1a379c93d44d3a85bf42f3d
hugomatheuss <hugo_matheuss@outlook.com>
2023-05-08 15:47:38 -0300

    Adicionando frontend.

---
+++ front/src/app/static/navbar-options.ts
@@ -0,0 +1,37 @@
+export const NavbarOptions = [
+    {
+        icon: '',
+        alt: 'Home',
+        label: 'ÍNICIO',
+        router: '/home',
+        needLogin: false
+    },
+    {
+        icon: '',
+        alt: 'Contas',
+        label: 'CONTAS',
+        router: '/contas',
+        needLogin: true
+    },
+    {
+        icon: '',
+        alt: 'Contatos',
+        label: 'CONTATO',
+        router: '/contato',
+        needLogin: true
+    },
+    {
+        icon: '',
+        alt: 'Login',
+        label: 'ENTRAR',
+        router: '/login',
+        needLogin: false
+    },
+    {
+        icon: '',
+        alt: 'register',
+        label: 'CADASTRAR',
+        router: '/register',
+        needLogin: false
+    }
+]
